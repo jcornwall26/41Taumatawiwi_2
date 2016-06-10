d8dd0aed896e375885757fc8d182393f79dcfaad
J Cornwall <jason.s.cornwall@gmail.com>
2016-06-10 15:29:42 -0700

    removed file

--- 41Taumatawiwi.Tests/InstagramPhotoRepositoryTests.cs
+++ 41Taumatawiwi.Tests/InstagramPhotoRepositoryTests.cs
@@ -8,6 +8,8 @@ namespace _41Taumatawiwi.Tests
     public class InstagramPhotoRepositoryTests
     {
         [TestMethod]
+        [TestCategory("Integration")]
+        [Timeout(1000)]
         public void Ensure_all_photos_can_be_retrieved()
         {
             var photoRepository = new InstagramPhotoRepository();
